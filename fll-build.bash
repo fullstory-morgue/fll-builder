905efe488a3f3d5574531077ebfd961c21e4f5d3
Kel Modderman <kel@otaku42.de>
2006-12-05 23:56:17 +0000

    more template stuff, no real code yet . . .

--- fll-build.bash
+++ fll-build.bash
@@ -1,2 +1,35 @@
 #!/bin/bash
 
+# Copyright (C) 2006 Sidux Crew, http://www.sidux.com
+#	Kel Modderman <kel@otaku42.de>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this package; if not, write to the Free Software 
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, 
+# MA 02110-1301, USA.
+#
+# On Debian GNU/Linux systems, the text of the GPL license can be
+# found in /usr/share/common-licenses/GPL.
+
+# fll-build(8): build a debian 'sid' live linux cd that uses code
+# from developed by members of the F.U.L.L.S.T.O.R.Y project to
+# enhance hardware detection and linux experience.
+#
+# F.U.L.L.S.T.O.R.Y Project Homepage:
+# http://developer.berlios.de/projects/fullstory
+#
+# F.U.L.L.S.T.O.R.Y Subversion Archive:
+# svn://svn.berlios.de/fullstory/trunk
+# http://svn.berlios.de/svnroot/repos/fullstory
+# http://svn.berlios.de/viewcvs/fullstory (viewcvs)
+# http://svn.berlios.de/wsvn/fullstory (websvn)
