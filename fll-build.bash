ae3f084ac714274607b68f0be85e0a913e2b1448
Kel Modderman <kel@otaku42.de>
2007-01-13 12:56:22 +0000

    add comment

--- fll-build.bash
+++ fll-build.bash
@@ -462,6 +462,8 @@ chroot_exec apt-get clean
 
 # clean apt lists
 find "$FLL_BUILD_CHROOT"/var/lib/apt/lists/ -not -name 'lock' -type f -exec rm -vf {} \;
+
+# clean /var
 find "$FLL_BUILD_CHROOT"/var/log/ -type f -exec rm -vf {} \;
 find "$FLL_BUILD_CHROOT"/var/run/ -type f -exec rm -vf {} \;
 
