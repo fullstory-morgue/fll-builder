0b8d257036d44de5e80e7f9772813e49a39bfa81
Kel Modderman <kel@otaku42.de>
2007-04-06 16:23:57 +0000

    fix typo, single pipe should have been double pipe

--- fll-build.bash
+++ fll-build.bash
@@ -310,7 +310,7 @@ for config in ${FLL_BUILD_CONFIGS[@]}; do
 		if [[ ${FLL_BUILD_EXTRAMIRROR_GPGKEYID[$i]} ]]; then
 			echo "Importing GPG key for ${FLL_BUILD_EXTRAMIRROR[$i]}"
 			chroot_exec gpg --keyserver wwwkeys.eu.pgp.net --recv-keys \
-				"${FLL_BUILD_EXTRAMIRROR_GPGKEYID[$i]}" | :
+				"${FLL_BUILD_EXTRAMIRROR_GPGKEYID[$i]}" || :
 		else
 			echo "Must provide GPG keyid for ${FLL_BUILD_EXTRAMIRROR[$i]}"
 			exit 6
