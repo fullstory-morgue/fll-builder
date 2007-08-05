75d260a2462eba42ee7c482a6ace8c5ecaef6ec8
Kel Modderman <kel@otaku42.de>
2007-08-05 00:46:49 +0000

    * determine absolute dirname of FLL_BUILD_AREA.

--- fll-build.bash
+++ fll-build.bash
@@ -232,16 +232,26 @@ while true; do
 	shift
 done
 
+#################################################################
+#		determine abs. dirname				#
+#################################################################
+absdirname() { [[ -n $1 ]] && ( pushd $1 >/dev/null && echo $PWD; ) }
+
 if [[ $1 ]]; then
-	FLL_BUILD_AREA="$1"
-	mkdir -p "$FLL_BUILD_AREA"
+	#
+	# make build dir and determine its absolute path
+	#
+	mkdir -p $1 && FLL_BUILD_AREA="$(absdirname $1)"
+	
+	#
+	# test for 'nodev' mount option
+	#
 	mknod "$FLL_BUILD_AREA"/test-dev-null c 1 3
-	if echo test > "$FLL_BUILD_AREA"/test-dev-null 2>/dev/null; then
-		rm -f "$FLL_BUILD_AREA"/test-dev-null
-	else
+	if ! echo testing 1 2 3 > "$FLL_BUILD_AREA"/test-dev-null 2>/dev/null; then
 		echo "$SELF: build area mounted with 'nodev' option, aborting"
 		exit 3
 	fi
+	rm -f "$FLL_BUILD_AREA"/test-dev-null
 else
 	echo "$SELF: must supply a build directory as first and only command line argument!"
 	echo
